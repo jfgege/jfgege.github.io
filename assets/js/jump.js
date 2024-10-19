faea28ec0231e4a50f51f06019e13b5453b671fa
jfgege <240902380@qq.com>
2024-10-19 18:36:24 +0800

    update-241019-1-main_add_pc_page

--- assets/js/jump.js
+++ assets/js/jump.js
@@ -6,15 +6,15 @@ function redirectBasedOnDevice() {
     var isMobile = /Mobi|Android|iPad/i.test(userAgent);
 
     // 判断是否为桌面设备
-    //var isDesktop = /Windows|Macintosh|Linux/i.test(userAgent) && !/Mobi|Android|iPad/i.test(userAgent);
+    var isDesktop = /Windows|Macintosh|Linux/i.test(userAgent) && !/Mobi|Android|iPad/i.test(userAgent);
 
     // 根据设备类型进行重定向
     if (isMobile) {
         // 如果是移动设备，跳转到移动版本的页面
         window.location.href = "mobile/index.html";
-    //} else if (isDesktop) {
+    } else if (isDesktop) {
         // 如果是桌面设备，跳转到桌面版本的页面
-        //window.location.href = "desktop/index.html";
+        window.location.href = "desktop/index.html";
     } else {
         // 对于所有其他设备，跳转到通用版本的页面
         window.location.href = "mobile/index.html";
--- assets/js/sidebar-mobile.js
+++ assets/js/sidebar-mobile.js
@@ -2,25 +2,22 @@ document.getElementById('sidebar-button').addEventListener('click', function() {
     var sidebar = document.getElementById('sidebar');
     if (sidebar.style.left === '0px') {
         sidebar.style.left = '-50rem';
+        document.querySelector('.overlay').style.opacity = '0';
+        setTimeout(function() {
+            document.querySelector('.overlay').style.visibility = 'hidden';
+    }, 300);
     } else {
         sidebar.style.left = '0px';
+        document.querySelector('.overlay').style.visibility = 'visible';
+        document.querySelector('.overlay').style.opacity = '1';
     }
 });
 
-// 可选：添加关闭按钮的点击事件
-document.getElementById('sidebar-close').addEventListener('click', function() {
+function closeSidebar() {
     var sidebar = document.getElementById('sidebar');
     sidebar.style.left = '-50rem';
-});
-
-
-/*
-// 为文档添加点击事件处理器，用于处理侧边栏的收回
-document.addEventListener('click', function(event) {
-    // 检查点击的元素是否为侧边栏或其子元素
-    if (!sidebar.contains(event.target)) {
-        // 如果点击的是侧边栏之外，将侧边栏收回
-        sidebar.style.left = '-40rem';
-    }
-});
-*/
+    document.querySelector('.overlay').style.opacity = '0';
+    setTimeout(function() {
+        document.querySelector('.overlay').style.visibility = 'hidden';
+    }, 300);
+}
