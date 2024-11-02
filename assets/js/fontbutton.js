9596890e3bcd6ef6ba2de88a397d0aa448c31140
jfgege <240902380@qq.com>
2024-11-02 20:11:42 +0800

    update-241102-1

--- assets/js/fontbutton.js
+++ assets/js/fontbutton.js
@@ -14,35 +14,3 @@ document.getElementById('fontList').addEventListener('click', function(event) {
     }
 });
 
-/*
-document.getElementById('fontToggle').addEventListener('click', function() {
-    var fontList = document.getElementById('fontList');
-    if (fontList.style.display === 'none') {
-        fontList.style.display = 'block';
-    } else {
-        fontList.style.display = 'none';
-    }
-});
-*/
-
-/*
-document.addEventListener('DOMContentLoaded', function() { //网页文档完全加载
-    var fontList = document.getElementById('fontList');
-    setTimeout(function() {
-        fontList.classList.add('visible'); // 0.5秒后使列表可见
-    }, 500);
-});
-*/
-/*
-document.getElementById('fontList').addEventListener('click', function(event) {
-    var target = event.target;
-    if (target.tagName === 'LI') {
-        var body = document.body;
-        body.style.fontFamily = target.getAttribute('data-font') + ', sans-serif';
-        var button = document.button;
-        button.style.fontFamily = target.getAttribute('data-font') + ', sans-serif';
-        document.getElementById('fontList').style.display = 'none';
-    }
-});
-*/
-
